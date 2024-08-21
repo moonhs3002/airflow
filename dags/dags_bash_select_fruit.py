4e8a17b120a53cdeccc50e1a9485395e16034399
moon <moonhs3002@gmail.com>
2024-08-21 17:23:48 +0900

    dags_bash_select_fruit mid

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -4,23 +4,22 @@ import pendulum
 from airflow.models.dag import DAG
 
 from airflow.operators.bash import BashOperator
-from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="dags_bash_select_fruit",
     schedule="10 0 * * 6#1",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
-) as DAG:
+) as gag:
     
     bash_orange = BashOperator(
         task_id="bash_orange",
-        bash_command="/opt/airflow/plugins/select_fruit.sh ORANGE"
+        bash_command="/opt/airflow/plugins/select_fruit.sh ORANGE",
     )
 
     bash_avocado = BashOperator(
         task_id="bash_avocado",
-        bash_command="/opt/airflow/plugins/select_fruit.sh AVOCADO"
+        bash_command="/opt/airflow/plugins/select_fruit.sh AVOCADO",
     )
 
 bash_orange >> bash_avocado

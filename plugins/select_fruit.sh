a36f8b0a047bc9311ad83c88ee3ba31cf09f7337
moon <moonhs3002@gmail.com>
2024-08-21 17:04:25 +0900

    plugins create

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,12 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ];then
+   echo "you selected Apple"
+elif [ $FRUIT == APPLE ];then
+   echo "you selected Apple"
+elif [ $FRUIT == ORANGE ];then
+   echo "you selected Orange"
+elif [ $FRUIT == GRAPE ];then
+   echo "you selected Grape"
+else
+   echo "you selected order Fruit"
+fi

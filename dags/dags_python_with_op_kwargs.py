5533952df308d5911f84209fdee5a03942e43f16
moon <moonhs3002@gmail.com>
2024-08-23 14:43:52 +0900

    kwargs test

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,23 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+
+from airflow.operators.python import PythonOperator
+from common.common_func import regist2
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist_t2 = PythonOperator(
+        task_id = 'regist_t2',
+        python_callable=regist2,
+        op_args=['moon','man','kr','seoul'],
+        op_kwargs={'email':'kkkk@penta.co.kr','phone':'01000000000'}
+        )
+    
+    regist_t2
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,3 +5,14 @@ def regist(name, sex, *args):
     print(f' name : {name}')
     print(f' sex  : {sex}')
     print(f' args : {args}')
+
+def regist2(name, sex, *args, **kwargs):
+    print(f'name : {name}')
+    print(f'sex  : {sex}')
+    print(f'args : {args}')
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)

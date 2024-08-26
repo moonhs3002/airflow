611da495e56fd5681caf13e917d68b710da1c255
moon <moonhs3002@gmail.com>
2024-08-26 14:30:36 +0900

    templat create

---
+++ dags/dags_bash_with_templat.py
@@ -0,0 +1,32 @@
+import datetime
+import pendulum
+import random
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_templat",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    bash_t1 = BashOperator(
+        task_id='bash_t1',
+        bash_command='echo "data_interval_end : {{ data_interval_end }} "'
+    )
+
+    bash_t2 = BashOperator(
+        task_id='bash_t2',
+        env={
+            'START_DATE':'{{data_interval_start | ds}}',
+            'END_DATE':'{{data_interval_end | ds}}'
+        },
+        bash_command='echo $START_DATE && $END_DATE'
+    )
+
+bash_t1 >> bash_t2
+
+bash_t1 >> bash_t2

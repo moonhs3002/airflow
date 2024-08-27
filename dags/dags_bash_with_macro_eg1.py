592f3d7bbd6795dceda3969353f67065a0bcc34b
moon <moonhs3002@gmail.com>
2024-08-27 15:50:43 +0900

    macro create

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -14,8 +14,8 @@ with DAG(
     #start_date:전월말일 end_date:1일전
     bash_task_1 = BashOperator(
         task_id='bash_task_1',
-        env={ 'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds}}',
-             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativetelta(days=1)) | ds }}'
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds}}',
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -14,8 +14,8 @@ with DAG(
     #start_date:2주전월요일 end_date:2주전토요일
     bash_task_2 = BashOperator(
         task_id='bash_task_2',
-        env={ 'START_DATE':'{{ (data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativetelta(days=19)) | ds }}',
-             'END_DATE':'{{ (data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativetelta(days=14)) | ds }}'
+        env={ 'START_DATE':'{{ (data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds }}',
+             'END_DATE':'{{ (data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds }}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )

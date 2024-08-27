34ef4197c8434588c3dba180ae8d213570f3e384
moon <moonhs3002@gmail.com>
2024-08-27 16:07:21 +0900

    macro create

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -7,7 +7,7 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_with_macro_eg2",
-    schedule="10 0 * * 6#2",
+    schedule="10 0 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:

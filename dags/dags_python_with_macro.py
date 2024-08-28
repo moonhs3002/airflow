0edc8f7534aa8e947b8debafb144ceff401387c9
moon <moonhs3002@gmail.com>
2024-08-28 14:50:00 +0900

    python macro create

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -20,7 +20,7 @@ with DAG(
 
     def get_templates_macro(**kwargs):
 
-        templates_dict = kwargs.get('templates_dict') or {}
+        templates_dict=kwargs.get('templates_dict') or {}
         if templates_dict:
             start_date = templates_dict.get('start_date') or 'start_date none'
             end_date = templates_dict.get('end_date') or 'end_date none'

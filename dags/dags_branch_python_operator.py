151eec847f127c9a5f9ae1f88fe9852e596052e8
moon <moonhs3002@gmail.com>
2024-08-30 13:43:06 +0900

    branch operator create

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -7,7 +7,7 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.python import BranchPythonOperator
 
 with DAG(
-    dag_id="dags_bash_with_variable",
+    dag_id="dags_branch_python_operator",
     schedule=None,
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False

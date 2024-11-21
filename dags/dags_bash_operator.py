eabd5239563272765c89622453cfaf9e33bfd26a
moon <moonhs3002@gmail.com>
2024-11-21 10:18:48 +0900

    bash_t3 task 추가

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -17,9 +17,15 @@ with DAG(
         bash_command="echo whoami",
     )
 
+
     bash_t2 = BashOperator(
         task_id="bash_t2",
         bash_command="echo $HOSTNAME",
     )
 
-bash_t1 >> bash_t2
+        bash_t3 = BashOperator(
+        task_id="bash_t3",
+        bash_command="echo $HOSTNAME",
+    )
+
+bash_t1 >> bash_t2 >> bash_t3
